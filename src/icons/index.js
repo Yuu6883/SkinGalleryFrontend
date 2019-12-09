75a7513330f47a198aac373249c66acadd8ad9ac
bigcupcoffee <bigcupcoffee@users.noreply.github.com>
2019-12-09 23:00:54 +0200

    fix retarded eslint error

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import icons from './iconsConfig';
 
-export const Icon = ({ name, ...rest }) => {
+const Icon = ({ name, ...rest }) => {
   const Use = icons[name];
   return <Use {...rest} />;
 };
